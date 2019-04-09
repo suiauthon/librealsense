01ce94328d980cef83a9b276be9713e62940aada
suiauthon <carek.marko@gmail.com>
2019-04-09 20:21:47 +0200

    changes in capture example

--- examples/capture/rs-capture.cpp
+++ examples/capture/rs-capture.cpp
@@ -27,10 +27,9 @@ int main(int argc, char * argv[]) try
 
     while (app) // Application still alive?
     {
-        printf("\n");
-        rs2::frameset data = pipe.wait_for_frames();//.    // Wait for next set of frames from the camera
-                             //apply_filter(printer).     // Print each enabled stream frame rate
-                             //apply_filter(color_map);   // Find and colorize the depth data
+        rs2::frameset data = pipe.wait_for_frames().    // Wait for next set of frames from the camera
+                             apply_filter(printer).     // Print each enabled stream frame rate
+                             apply_filter(color_map);   // Find and colorize the depth data
         // The show method, when applied on frameset, break it to frames and upload each frame into a gl textures
         // Each texture is displayed on different viewport according to it's stream unique id
         app.show(data);
