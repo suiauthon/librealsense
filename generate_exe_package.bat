5a05e07b3123233f607c5648d65c14acc83b8d2b
Dominik Zebec <dominik.zebec@smartekvision.com>
2019-08-23 09:50:53 +0200

    updated package generation script for windows

--- generate_exe_package.bat
+++ generate_exe_package.bat
@@ -10,12 +10,14 @@ CD %PWD%
 CALL:READARGUMENTS %*
 CALL:INIT
 
-MKDIR bin\%TARGET_SYSTEM% 2> nul
-
 MKDIR build\%TARGET_SYSTEM% 2> nul
 CD build\%TARGET_SYSTEM%
 
+cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_BUILD_TYPE=Release -DCPACK_SYSTEM_NAME=Linux64_x66 ../../
 
+CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
+msbuild PACKAGE.vcxproj /t:Build /p:Configuration=Release
 
 goto:eof
 
