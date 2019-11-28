25bfdabae47e034b5401f9ac30911e0ab003e8d1
Dominik Zebec <dominik.zebec@smartekvision.com>
2019-11-28 14:17:23 +0100

    integrated dynamic calibration tool

--- CMake/install_config.cmake
+++ CMake/install_config.cmake
@@ -62,7 +62,15 @@ install(FILES
 )
 
 if (UNIX)
-	set(DYNAMIC_CALIBRATOR_DIR TODO)
+    set(DYNAMIC_CALIBRATOR_DIR /opt/Projects/RS-D4-ETH/Software/RS-CalibrationToolAPI/linux/usr)
+    install(FILES
+        ${DYNAMIC_CALIBRATOR_DIR}/bin/DynamicCalibrator
+        DESTINATION ${CMAKE_INSTALL_BINDIR}    
+    )
+    install(FILES
+        ${DYNAMIC_CALIBRATOR_DIR}/lib/libDSDynamicCalibrationAPI.so
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    )
 elseif (WIN32)
 	set(DYNAMIC_CALIBRATOR_DIR C:/SmartekVision/Projects/RS-D4-ETH/Software/RS-CalibrationToolAPI/windows/bin)
 	install(FILES 
