24c0c69914db28c97709e7cf994df3bf47f3302e
Dominik Zebec <d.zebec@framos.com>
2019-12-19 10:19:03 +0100

    removed dynamic calibrator installation from deployed source

--- CMake/install_config.cmake
+++ CMake/install_config.cmake
@@ -62,27 +62,10 @@ install(FILES
 )
 
 if (UNIX)
-    set(DYNAMIC_CALIBRATOR_DIR /opt/Projects/RS-D4-ETH/Software/RS-CalibrationToolAPI/linux/usr)
-    install(PROGRAMS
-        ${DYNAMIC_CALIBRATOR_DIR}/bin/DynamicCalibrator
-        DESTINATION ${CMAKE_INSTALL_BINDIR}    
-    )
-    install(FILES
-        ${DYNAMIC_CALIBRATOR_DIR}/lib/libDSDynamicCalibrationAPI.so
-        DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    )
     install(FILES
         ${PROJECT_SOURCE_DIR}/config/99-realsense-libusb.rules 
         DESTINATION /etc/udev/rules.d/
     )
-elseif (WIN32)
-	set(DYNAMIC_CALIBRATOR_DIR C:/SmartekVision/Projects/RS-D4-ETH/Software/RS-CalibrationToolAPI/windows/bin)
-	install(FILES 
-		${DYNAMIC_CALIBRATOR_DIR}/DynamicCalibrator.exe
-		${DYNAMIC_CALIBRATOR_DIR}/DSDynamicCalibrationAPI.dll
-		${DYNAMIC_CALIBRATOR_DIR}/freeglut.dll
-		DESTINATION ${CMAKE_INSTALL_BINDIR}
-	)
 endif()
 
 install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/librealsense2
---
+++ install_dynamic_calibrator.cmake
@@ -0,0 +1,19 @@
+if (UNIX)
+    set(DYNAMIC_CALIBRATOR_DIR /opt/Projects/RS-D4-ETH/Software/RS-CalibrationToolAPI/linux/usr)
+    install(PROGRAMS
+        ${DYNAMIC_CALIBRATOR_DIR}/bin/DynamicCalibrator
+        DESTINATION ${CMAKE_INSTALL_BINDIR}    
+    )
+    install(FILES
+        ${DYNAMIC_CALIBRATOR_DIR}/lib/libDSDynamicCalibrationAPI.so
+        DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    )
+elseif (WIN32)
+	set(DYNAMIC_CALIBRATOR_DIR C:/SmartekVision/Projects/RS-D4-ETH/Software/RS-CalibrationToolAPI/windows/bin)
+	install(FILES 
+		${DYNAMIC_CALIBRATOR_DIR}/DynamicCalibrator.exe
+		${DYNAMIC_CALIBRATOR_DIR}/DSDynamicCalibrationAPI.dll
+		${DYNAMIC_CALIBRATOR_DIR}/freeglut.dll
+		DESTINATION ${CMAKE_INSTALL_BINDIR}
+	)
+endif()
