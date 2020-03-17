ab5b818d73bc8ce35e78692300b49f373d5b8f3b
Dominik Zebec <d.zebec@framos.com>
2020-03-17 13:46:44 +0100

    fixed copying .git folder into install

--- install_ros_wrapper.cmake
+++ install_ros_wrapper.cmake
@@ -1,8 +1,9 @@
 if (UNIX)
     if ( NOT DEFINED ENV{ROS_WRAPPER_PATH} )
-        message( FATAL_ERROR "ROS_WRAPPER_PATH not deinfed" )
+        message( FATAL_ERROR "ROS_WRAPPER_PATH not defined" )
     endif()
     install(DIRECTORY
+		PATTERN ".git" EXCLUDE
         $ENV{ROS_WRAPPER_PATH}/
         DESTINATION src/librealsense2/wrappers/ros/
     )
