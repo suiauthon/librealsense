3ebc108ce9bc196eb0eac4bc4ac6d4a1f9f257b5
Dominik Zebec <d.zebec@framos.com>
2020-04-06 13:44:15 +0200

    exported setting buffers count in Python

--- wrappers/python/pyrs_d400e.cpp
+++ wrappers/python/pyrs_d400e.cpp
@@ -9,5 +9,7 @@ void init_d400e(py::module& m) {
     auto d400e_submodule = m.def_submodule("d400e", "Functionality specific for D400e series devices");
     d400e_submodule.def("set_heartbeat_time", &rs2::d400e::set_heartbeat_time, "set heartbeat time for d400e devices");
     d400e_submodule.def("get_heartbeat_time", &rs2::d400e::get_heartbeat_time, "retrieve heartbeat time for d400e devices");
+    d400e_submodule.def("set_buffer_count", &rs2::d400e::set_buffer_count, "set number of buffers for d400e devices");
+    d400e_submodule.def("get_buffer_count", &rs2::d400e::get_buffer_count, "get number of buffers for d400e devices");
     /** end rs_d400e.hpp **/
 }
