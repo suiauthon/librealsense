c3c6d0e5505d219830be5d6e2f067d2a568ca389
Dominik Zebec <d.zebec@framos.com>
2020-04-06 13:26:24 +0200

    fixed exporting C and C++ buffer count API, exported buffer count API in C#

--- wrappers/csharp/Intel.RealSense/NativeMethods.cs
+++ wrappers/csharp/Intel.RealSense/NativeMethods.cs
@@ -567,7 +567,13 @@ namespace Intel.RealSense
 
         [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void rs2_d400e_set_heartbeat_time(double time, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ErrorMarshaler))] out object error);
-        
+
+        [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int rs2_d400e_get_buffer_count([MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ErrorMarshaler))] out object error);
+
+        [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void rs2_d400e_set_buffer_count(int buffer_count, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(ErrorMarshaler))] out object error);
+
 #endregion
 #region rs_advanced_mode
         [DllImport(dllName, CallingConvention = CallingConvention.Cdecl)]
